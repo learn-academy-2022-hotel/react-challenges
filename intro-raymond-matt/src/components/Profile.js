24b95ecbcb2421570c2fa19f8b2b53f6ae287f97
RaymondOchoa <ochoaraymondb@yahoo.com>
2022-12-09 13:44:58 -0800

    completed react challenges

---
+++ intro-raymond-matt/src/components/Profile.js
@@ -0,0 +1,15 @@
+import React from 'react';
+const Profile = () =>  {
+    return (
+      <>
+        <h2>About Raymond</h2>
+        <p>I'm 24 and from Sacramento, CA </p>
+        <h2>About Matt</h2>
+        <p>I'm 37 and from San Diego, CA</p>
+      </>
+     
+    );
+  }
+  
+  export default Profile;
+  
