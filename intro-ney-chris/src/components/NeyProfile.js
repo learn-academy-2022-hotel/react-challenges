f18df93e910f3330704bf8c2f93df4f5f2256e64
chrisyhlee <leechr8393@gmail.com>
2022-12-09 12:55:38 -0800

    challenges for react intro

---
+++ intro-ney-chris/src/components/NeyProfile.js
@@ -0,0 +1,15 @@
+const NeyProfile = () => {
+    return (
+    <>
+    <h3>Ney</h3>
+    <ul>
+        <li>Age: 24</li>
+        <li>Height: 6'0</li>
+        <li>Weight: 185lbs</li>
+        <li>Race: Filipino</li>
+    </ul>
+    </>
+    )
+}
+
+export default NeyProfile
