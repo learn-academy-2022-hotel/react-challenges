da7db24cf1535ef09ae07075696dc5e02560bd6e
nvencer98 <vencer98@gmail.com>
2022-12-12 13:41:12 -0800

    finished first challenge queation

---
+++ state-christian-ney/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+  <div className="box"> 
+  white
+    <li className="button">
+    </li>
+    </div>
+  );
+}
+
+export default App;
