cf7ea882cd698094068736a36f75f54a204562af
chrisyhlee <leechr8393@gmail.com>
2022-12-12 15:56:08 -0800

    challenges for react state and part of stretch

---
+++ state-fred-octavio-chris/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Box from './components/Box';
+import ButtonToAdd from './components/ButtonToAdd';
+import { useState } from 'react';
+import ButtonToRemove from './components/ButtonToRemove';
+
+function App() {
+  // state = {
+  //   components: []
+  // }
+  return (
+    <>
+      <ButtonToAdd/>
+      {/* <ButtonToRemove/> */}
+    </>
+  )
+}
+
+
+
+export default App;
