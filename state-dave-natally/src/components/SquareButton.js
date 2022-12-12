82daef15bd8386f4b4570b381c68f265b08ff5d1
Nata19754 <chavesnatally0419@gmail.com>
2022-12-12 14:03:20 -0800

    added info

--- state-dave-natally/src/components/SquareButton.js
+++ state-dave-natally/src/components/SquareButton.js
@@ -2,18 +2,20 @@ import React, { useState } from "react"
 
 const Square = () => {
     const [color, setColor] = () => useState(false)
-    const handleClik = () => {
-        setColor(!active)
+    const handleClick = () => {
+        setColor(!color)
     }
   
     return (
       <>
         <button onClick={handleClick}
-            style={{
-            height: "100px", 
-            width: "100px" , 
-            borderColor: "black", 
-            backgroundColor: active ? "white" : "black" }}> White </button>
+            className = {`px-20 py-10 text-3x1 capitalized font-bold text-white rounded-lg ${(color === true)? 'bg-green-700' : 'bg-blue-700'}`}
+            > white </button>
+
+            {/* // height: "100px", 
+            // width: "100px" , 
+            // borderColor: "black", 
+            // backgroundColor: active ? "white" : "black" }}> White </button> */}
       </>
     )
   }
