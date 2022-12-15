1cbd8379b252fdade1e7e5333ddda52526b39094
lmhazel <leahazel2000@gmail.com>
2022-12-14 16:28:57 -0800

    attempted challenges

---
+++ props-monica-fred-lea/src/components/RollDice.js
@@ -0,0 +1,13 @@
+import REACT from 'react';
+
+const RollDice = (props) => {
+    //console.log(props)
+    return( 
+        <>
+        {/* console.log(Math.random() * 6) */}
+        {props.number}
+        </>
+    )
+    }
+
+export default RollDice
