20f87a860d2dcbc2f2ddd42a24d5d5affad0443f
chrisyhlee <leechr8393@gmail.com>
2022-12-14 16:43:25 -0800

    completed challenges for props

---
+++ props-raquel-chris/src/components/RandomDice.js
@@ -0,0 +1,8 @@
+import React, {useState} from "react";
+
+// const RandomDice = () => {
+//     let diceRolls = [1, 2, 3, 4, 5, 6]  
+//     return Math.floor(Math.random(diceRolls) * 7)
+// }
+
+// export default RandomDice
